93d58919bc69037bb272e8e0f5e8dddd51ca76d7
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2023-07-29 15:40:19 -0700

    Set up VC and Text-Channel dedicated to Client operation.

--- Controller.py
+++ Controller.py
@@ -4,20 +4,58 @@ import discord
 from dotenv import load_dotenv
 import pyautogui, pydirectinput
 
+# Set-up Client connection, get channel ID's for text and voice.
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+TXT_ID = os.getenv("TXT_CHANNEL_ID")
+VC_ID = os.getenv("VOICE_CHANNEL_ID")
 
-client = discord.Client(intents=discord.Intents.default())
+client = discord.Client(intents=discord.Intents.all())
+
+# TODO: Initialize on_ready embed text and response for controls prompt.
+controls_desc = ("__**Controls**__: \n " +
+                 ""
+                )
+
+txt_channel, vc_channel = None, None
 
 @client.event
 async def on_ready():
+    # Set up channel references
+    txt_channel = client.get_channel(int(TXT_ID))
+    vc_channel = client.get_channel(int(VC_ID))
+    print(type(txt_channel))
+
     print("Controller is connected!")
-    print("Automatically print controls with their corresponding text commands here")
+    embed = discord.Embed(title="Controller Connected!", description=f"*Connected to text channel:* **{txt_channel.name}** \n *Connected to voice channel:* **{vc_channel.name}**",color=discord.Color.dark_green())
+    await txt_channel.send(embed=embed)
+
 
 @client.event
 async def on_message(message):
-    print("Read user input, only respond if coming from a dedicated channel")
-    print("Interface with pydirect input to move character in game")
+    # Restrict response to messages only within dedicated channel
+    if message.channel.id != int(TXT_ID):
+        return
+    
+    # Avoid message/input loop
+    if message.author == client.user:
+        return
+    
+    print(message.content)
+
+    # TODO: Interface input with controls to control host game
+    # print("Interface with pydirect input to move character in game")
+
+# Error handling when received unhandled message.
+@client.event
+async def on_error(event, *args, **kwargs):        
+    with open("errors.log", "a") as f:
+        if event == "on_message":
+            f.write(f"Error with message: {args[0]}\n")
+        else:
+            raise # Resort to default error handler
+
+
 
 client.run(TOKEN)
 
