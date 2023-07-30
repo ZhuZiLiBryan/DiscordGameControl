a5efd1fc9386900a43aaabd590f63b009b4bcb69
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2023-07-29 21:14:39 -0700

    Set up skeleton for controls; chat messages can now control host game

--- Controller.py
+++ Controller.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import time
 
 from dotenv import load_dotenv
 import pyautogui, pydirectinput
@@ -13,9 +14,30 @@ VC_ID = os.getenv("VOICE_CHANNEL_ID")
 client = discord.Client(intents=discord.Intents.all())
 
 # TODO: Initialize on_ready embed text and response for controls prompt.
+# TODO: Allow to read from CSV file to customize control, and allow versatile controls_desc to reflect new control schemes
 controls_desc = ("__**Controls**__: \n " +
-                 ""
+                 "**Move Up:** u \n" +
+                 "**Move Down:** d \n" +
+                 "**Move Left:** l \n" +
+                 "**Move Right:** r \n" +
+                 "**Z:** z \n" +
+                 "**X:** x \n" +
+                 "**C:** c \n"
                 )
+
+# Hashmap to correspond command with keycode
+control_map = {
+    "u": "up",
+    "d": "down",
+    "l": "left",
+    "r": "right",
+    "z": "z",
+    "x": "x",
+    "c": "c"
+}
+
+# Can adjust time a key is held down for 
+hold_time = 0.5
 
 txt_channel, vc_channel = None, None
 
@@ -24,27 +46,33 @@ async def on_ready():
     # Set up channel references
     txt_channel = client.get_channel(int(TXT_ID))
     vc_channel = client.get_channel(int(VC_ID))
-    print(type(txt_channel))
 
     print("Controller is connected!")
     embed = discord.Embed(title="Controller Connected!", description=f"*Connected to text channel:* **{txt_channel.name}** \n *Connected to voice channel:* **{vc_channel.name}**",color=discord.Color.dark_green())
     await txt_channel.send(embed=embed)
+    await txt_channel.send(controls_desc)
 
 
 @client.event
 async def on_message(message):
     # Restrict response to messages only within dedicated channel
-    if message.channel.id != int(TXT_ID):
+    if message.channel.id != int(TXT_ID) or message.author.voice.channel.id != int(VC_ID):
         return
     
     # Avoid message/input loop
     if message.author == client.user:
         return
     
-    print(message.content)
+    # Print control scheme with commands
+    if message.content == "controls":
+        await message.channel.send(controls_desc)
 
-    # TODO: Interface input with controls to control host game
-    # print("Interface with pydirect input to move character in game")
+    # Interface input with controls to control host game
+    if message.content in control_map:
+        with pydirectinput.press(control_map[message.content]):
+            time.sleep(hold_time)
+        
+        
 
 # Error handling when received unhandled message.
 @client.event
